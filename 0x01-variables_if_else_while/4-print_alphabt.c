4b1ef6e094870e8ebf259a7efd38afc5d9f296fd
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-17 04:21:14 -0700

    4-print_alphabt adding

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
