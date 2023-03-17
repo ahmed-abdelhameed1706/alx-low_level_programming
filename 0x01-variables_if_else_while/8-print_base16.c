da0149caf27b86226de913e0b186c47ca7bfa43e
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-17 04:42:12 -0700

    8-print_base16 fixing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- this is the main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
