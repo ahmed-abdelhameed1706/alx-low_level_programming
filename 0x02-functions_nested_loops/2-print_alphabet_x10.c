4e35554d800f608fe920ea595b4e0f6695fb5872
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-21 04:32:15 -0700

    2-print_alphabet_x10.c fixing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - prints the alphapet x10
+ * print_alphabet_x10 - prints the alphapet x10
  *
  * Return: nothing
  */
@@ -21,5 +21,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		x++;
 	}
-	
 }
