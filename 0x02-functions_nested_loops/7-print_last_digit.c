98ab533aaa67ac9855456c0f87610c38af91fc5b
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-21 08:41:36 -0700

    7-last_digit adding

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * @i: parameter to be used
+ *
+ * Return: returns last digit
+ */
+int print_last_digit(int i)
+{
+	int last = i % 10;
+
+	return (last);
+}
