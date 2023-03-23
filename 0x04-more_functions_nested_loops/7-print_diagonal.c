db793951d2847a7dfa36480ad903aa5b14c1bf55
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-23 07:15:45 -0700

    7-print_diagonal.c adding

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints line
+ * @n: parameter to be used
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
