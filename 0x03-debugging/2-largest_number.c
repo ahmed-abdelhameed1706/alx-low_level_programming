7798b27e7df6c0f5e486e967bf2b2d95d68e7855
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-23 09:34:47 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
 	else if (a > c && c > b)
 	{
 		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
 	}
 	else if (b > c && c > a)
 	{
