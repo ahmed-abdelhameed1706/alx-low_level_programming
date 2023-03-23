7341ffddf431c8362cf8cee27904f42b1807d2b1
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-23 08:33:54 -0700

    10-print_triangle.c adding

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints line
+ * @size: parameter to be used
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
