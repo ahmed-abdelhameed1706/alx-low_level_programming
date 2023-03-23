bcaedd5a845c4cade8d6f37cb9802bc4bef66524
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-23 06:34:25 -0700

    6-print_line.c adding

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: parameter to be used
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
