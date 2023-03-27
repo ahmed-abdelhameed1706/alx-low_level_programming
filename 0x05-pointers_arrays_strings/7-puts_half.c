a1deb592c0df5fd584ad2ab1d53b998bffed8a8e
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-27 15:32:48 -0700

    adding fiels

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - main
+ * @str: parameter
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 != 0)
+	{
+		j = (i - 1) / 2;
+	}
+	else
+	{
+		j = i / 2;
+	}
+
+	for (k = j; k < i; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
