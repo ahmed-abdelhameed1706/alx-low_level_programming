bc96700f2e1fd79ebf1d33e56cc1a12f75443a19
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-03-27 16:00:26 -0700

    adding fiels

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - main
+ * @dest: param
+ * @src: param
+ * return : char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int srcLen = 0;
+	int k;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		srcLen++;
+	}
+
+	for (k = 0; k < srcLen; k++)
+	{
+		dest[k] = src[k];
+	}
+	dest[srcLen] = '\0';
+	return (dest);
+}
