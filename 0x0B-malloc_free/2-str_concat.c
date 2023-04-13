3e9a3cbeb5ea3974806d6d09f8c0ee2259dff0eb
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-04-12 17:23:24 -0700

    adding files

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - main function
+ * @s1: parameter
+ * @s2: parameter
+ * Return char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int s1_size, s2_size = 0;
+	char *result;
+	int i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = '\0';
+	}
+
+	while (s1[s1_size] != '\0')
+	{
+		s1_size++;
+	}
+
+	while (s2[s2_size] != '\0')
+	{
+		s2_size++;
+	}
+
+	result = malloc(s1_size + s2_size + 1);
+
+	if (result == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < s1_size; i++)
+	{
+		result[i] = s1[i];
+	}
+
+	for (j = i; j < s2_size + s1_size; j++)
+	{
+		result[j] = s2[j - i];
+	}
+
+	result[j] = '\0';
+
+	return (result);
+}
