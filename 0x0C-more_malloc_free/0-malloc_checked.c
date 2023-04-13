ac9c50fc146fab9fbf3b2e5945876ae182fe5c59
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-04-12 21:05:52 -0700

    adding files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *malloc_checked - main function
@@ -8,7 +9,7 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *result;
+	void *result;
 
 	result = malloc(b);
 
