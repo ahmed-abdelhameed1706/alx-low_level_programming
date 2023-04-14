b8a74702ddc6e5622bac10d3acf09b270d3cef3d
Ahmed Abd El Hameed <ahmed.abdelhameed1706@gmail.com>
2023-04-13 18:19:41 -0700

    adding files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * *string_nconcat - concates 2 strings
  * @s1: parameter string 1
@@ -8,49 +7,41 @@
  * @n: how many bytes to concat
  * Return: pointer to char
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
 	unsigned int len1 = 0, len2 = 0, i, j;
 
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
+	if (s1 == NULL)
+		s1 = "";
 
-	result = malloc(len1 + n + 1);
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0' && len2 < n)
+		len2++;
+
+	result = malloc(len1 + len2 + 1);
 
 	if (result == NULL)
-	{
 		return (0);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		result[i] = s1[i];
-	}
-	if (n >= len2)
-	{
-		for (j = 0; j < len2; j++)
-		{
-			result[i + j] = s2[j];
-		}
-		result[i + j] = '\0';
-	}
-	else
-	{
-		for (j = 0; j < n; j++)
-		{
-			result[i + j] = s2[j];
-		}
-		result[i + j] = '\0';
 
-	}
+	for (j = 0; j < len2; j++)
+		result[i + j] = s2[j];
+
+	result[i + j] = '\0';
+
+
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+
+	result[i + j] = '\0';
 	return (result);
 
 }
