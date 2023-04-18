d270362c7d2d1d8cd247f84b543cdeb63c4c0d20
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-17 22:12:29 -0700

    adding files

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - function
+ * @ac: parameter
+ * @av: parameter
+ * Return: Char
+ */
+char *argstostr(int ac, char **av)
+{
+	int len = 0, k = 0;
+	int i, j;
+	char *result;
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+		len++;
+	}
+	len++;
+
+	result = malloc(len);
+
+	if (result == NULL)
+		return (0);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			result[k] = av[i][j];
+			j++;
+			k++;
+		}
+		result[k] = '\n';
+		k++;
+	}
+	result[k] = '\0';
+
+	return (result);
+}
