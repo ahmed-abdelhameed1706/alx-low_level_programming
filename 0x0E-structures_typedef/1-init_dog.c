b5b0ec21a3d54213e30c0ee92f99553282b6840e
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-17 18:46:13 -0700

    adding files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 
 /**
  * init_dog - main function
@@ -10,10 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,13 @@
  * @owner: variable
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
