0f190bc9097c8e4c917415d44b7448c089ee2aae
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-17 18:30:42 -0700

    adding files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - make a new type of object dog
+ * @name: variable
+ * @age: variable
+ * @owner: variable
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
