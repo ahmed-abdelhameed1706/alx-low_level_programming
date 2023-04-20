8caa8fac1f9807f8532c07aac383d61eebcafa56
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-19 18:47:03 -0700

    adding files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - main function
+ * @f: parameter
+ * @name: parameter
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
