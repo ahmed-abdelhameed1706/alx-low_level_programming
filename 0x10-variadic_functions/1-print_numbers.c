9ed8fc143269c23ea347c8e9ab2c5ede0fb7529c
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-20 16:36:07 -0700

    adding files

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - function
+ * @separator: parameter
+ * @n: parameter
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(args, int));
+
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
