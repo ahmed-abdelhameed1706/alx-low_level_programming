e542e0d6df7fe4e92199c9aee552996c65baf864
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-20 17:14:53 -0700

    adding files

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_all - function
+ * @format: parameter
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	char *s;
+	int i = 0;
+	va_list args;
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				printf("%s", s != NULL ? s : "(nil)");
+				break;
+			default:
+				break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'f' || format[i] == 'i' || format[i] == 's'))
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
