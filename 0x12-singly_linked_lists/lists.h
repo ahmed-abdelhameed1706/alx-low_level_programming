609e64f80a9c0488e108491436bc9df775def0bd
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-27 09:30:25 -0700

    fixed print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,8 +20,13 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/*FUNCTION PROTOTYPES*/
+
 /**
- * Function Prototypes
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
  */
 int _putchar(char c);
 size_t print_list(const list_t *h);
