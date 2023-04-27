1a127eebc3f2f43ce71216d17f48a4bbe4c32150
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-04-27 09:38:24 -0700

    adding list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * list_len - function to get the lengh of list elements
+ * @h: a list passed to the function
+ *
+ * Return: number of elements of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t list_length = 0;
+
+	while (h != NULL)
+	{
+		list_length++;
+		h = h->next;
+	}
+	return (list_length);
+}
