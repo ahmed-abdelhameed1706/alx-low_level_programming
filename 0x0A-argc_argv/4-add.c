86164ed59712c75f9e408e59b3c35ba928f1630d
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 04:01:08 -0700

    adding add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * custom_atoi - my atoi function
+ * @str: The string to convert to an integer
+ *
+ * Return: int
+ */
+int custom_atoi(const char *str)
+{
+	int num = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (-1); /* Not a valid number */
+		}
+
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (num);
+}
+
+/**
+ * main - main function
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		int num = custom_atoi(argv[i]);
+
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int main(int argc, char *argv[]);
+int custom_atoi(const char *str);
 
 #endif
