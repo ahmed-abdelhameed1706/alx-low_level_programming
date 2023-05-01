3039783f80a13a0120efb589f4cb907cb07c763c
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 03:06:30 -0700

    fixing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,9 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	
-	if (*head == NULL)
-		return (NULL);
-
 	new_node->n = n;
 	new_node->next = NULL;
 
@@ -35,6 +32,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	for (i = 0; i < idx - 1 && temp != NULL; i++)
 		temp = temp->next;
+	
+	if (temp == NULL)
+		return (NULL);
 
 	new_node->next = temp->next;
 	temp->next = new_node;
