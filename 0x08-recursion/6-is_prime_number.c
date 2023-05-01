5fbefdc7a64327191199cdc6ddc2ee93cf5e41c4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 04:14:24 -0700

    adding 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+/**
+ * is_prime - checks if n is a prime number
+ * @n: the number to check
+ * @i: the current factor being tested
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+
+	return (is_prime_number_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if n is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime_number_helper(n, 2));
+}
