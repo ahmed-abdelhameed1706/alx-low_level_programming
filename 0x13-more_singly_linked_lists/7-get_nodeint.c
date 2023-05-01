1b49f60e7ef739d06e0b9410333e0ae8425f9bba
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 02:34:09 -0700

    fixing

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index && temp != NULL; i++)
 	{
-		temp = temp->next
+		temp = temp->next;
 	}
 	if (temp == NULL)
 		return (NULL);
