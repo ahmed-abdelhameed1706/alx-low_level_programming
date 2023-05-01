d4c1ba7b3481ce5924236d93dfe8a1eebaeb24e7
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 03:46:51 -0700

    fixig

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,10 @@
-section .data
-    format db "Hello, Holberton!\n", 0
-
-section .text
-    global _start
-
-_start:
-    ; prepare arguments for printf
-    push format
-    call printf
-    add rsp, 8 ; clean up the stack
-
-    ; exit the program
-    mov eax, 60 ; system call for exit
-    xor edi, edi ; return 0
-    syscall
+	global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
