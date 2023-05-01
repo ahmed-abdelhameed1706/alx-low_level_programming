1fa40a0ef134ae08457dbc5d691553a71e8fccf0
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 01:57:22 -0700

    adding free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: pointer to the list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		head = (*head->)next;
+		free(temp);
+	}
+	*head = NULL;
+}
