dab3832150045e5783462a850367cf90eaf11ac0
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 00:16:11 -0700

    adding print_listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,31 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Function Prototypes */
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
