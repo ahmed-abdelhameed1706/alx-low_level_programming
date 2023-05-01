38eb26c88d5aef60e14e69e1527f824ba41f51db
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 03:01:43 -0700

    fixing inserting node at position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,14 +13,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i;
 
-	if (idx = 0)
-		return (add_nodeint(head, n));
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
+	if(new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	temp = *head;
 	for (i = 0; i < idx - 1 && temp != NULL; i++)
