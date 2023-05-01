41d1d6bd3628eb397eb4a18583eea916381b5a31
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 04:15:48 -0700

    fixing

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,7 +15,7 @@ int is_prime(int n, int i)
 	if (n % i == 0)
 		return (0);
 
-	return (is_prime_number_helper(n, i + 1));
+	return (is_prime(n, i + 1));
 }
 
 /**
@@ -27,5 +27,5 @@ int is_prime(int n, int i)
 
 int is_prime_number(int n)
 {
-	return (is_prime_number_helper(n, 2));
+	return (is_prime(n, 2));
 }
