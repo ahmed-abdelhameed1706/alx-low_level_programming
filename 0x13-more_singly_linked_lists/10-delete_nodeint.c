e60f703c6d1aa734032747bc7dd00b1719758386
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-01 03:23:53 -0700

    adding delet node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at index;
+ * @head: pointer to the first node
+ * @index: index of the node to be deleted
+ *
+ * Return: 1 on succes -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp;
+	listint_t *to_be_deleted;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		to_be_deleted = *head;
+		*head = (*head)->next;
+		free(to_be_deleted);
+		return (1);
+	}
+
+	temp = *head;
+
+	for (i = 0; i < index - 1 && temp != NULL; i++)
+		temp = temp->next;
+
+	if (temp == NULL || temp->next == NULL)
+		return (-1);
+
+	to_be_deleted = temp->next;
+	temp->next = to_be_deeletd->next;
+	free(to_be_deleted);
+
+	return (1);
+}
