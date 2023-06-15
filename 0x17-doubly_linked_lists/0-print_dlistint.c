b1523ca33072771176935490a1aff1d4426e383b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-15 06:12:43 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,17 +2,18 @@
 
 /**
  * print_dlistint - prints the entire list
- * @dlistint_t: parameter
+ * @h: parameter
  *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *temp;
+
 	temp = h;
 	size_t count;
 
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
