d1c55b15c6cab0cedc844050e517e02033a86009
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-06 22:52:34 -0700

    fixing everything?

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int in;
+	unsigned long int i;
 
-	table = malloc(sizeof(hast_table_t));
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
