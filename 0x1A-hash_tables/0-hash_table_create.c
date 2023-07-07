7f502c96ef1e0f8ae37b134f33fb3a4f40003d34
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-06 23:00:05 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * (table->size));
+	table->array = calloc(table->size, sizeof(hash_node_t));
 
 	if (table->array == NULL)
 	{
