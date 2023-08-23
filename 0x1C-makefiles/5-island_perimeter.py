19ed03036aefe45338077bd51fbd6109be0312ec
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-23 11:28:43 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-Module to calculate parimeter of island 
+Module to calculate parimeter of island
 """
+
+
 def island_perimeter(grid):
     """
     function to calcuate the parimeter
     """
     size = 0
-    #width = len(grid[0])
-    #length = len(grid)
+    edges = 0
+    width = len(grid[0])
+    length = len(grid)
 
-    for i in grid:
-        for j in i:
-            if j == 1:
+    for i in range(length):
+        for j in range(width):
+            if grid[i][j] == 1:
                 size += 1
 
-    edges = (size - 1) * 2
-    parimeter = (size * 4) - edges
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
+
+    parimeter = (size * 4) - (edges * 2)
 
     return parimeter
